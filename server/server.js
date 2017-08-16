343e3a3e78e18928977293a389afec882658c5f3
Charirixenstrix <charirixenstrix@gmail.com>
2017-08-16 09:45:11 +0200

    Add post /todos route and refactor mongoose

--- server/server.js
+++ server/server.js
@@ -1,43 +1,35 @@
-var mongoose=require('mongoose');
+var express=require('express');
+var bodyParser=require('body-parser');
 
-mongoose.Promise=global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp', {
-  useMongoClient: true
+var {mongoose}=require('./db/mongoose');
+var {Todo}=require('./models/todo');
+var {User}=require('./models/user');
+
+var app=express();
+app.use(bodyParser.json());
+
+app.post('/todos', (req, res)=>{
+  console.log(req.body);
+  var todo = new Todo({
+    text: req.body.text
+  });
+  todo.save().then((doc)=>{
+    res.send(doc);
+  }, (e)=>{
+    res.status(400).send(e);
+  });
 });
 
-var Todo=mongoose.model('Todo', {
-  text: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  },
-  completed: {
-    type: Boolean,
-    default: false,
-
-  },
-  completedAt: {
-    type: Number,
-    default: null
-  }
+app.listen(3000, ()=>{
+  console.log('Server started');
 });
 
-var User=mongoose.model('User', {
-  email: {
-    type: String,
-    required: true,
-    minlength: 1,
-    trim: true
-  }
-});
-
-var newUser=new User({email: 'emailcím'});
+/*var newUser=new User({email: 'emailcím'});
 newUser.save().then((doc)=>{
   console.log('Elmentve', doc);
 }, (e)=>{
   console.log('Nem sikerült menteni', e);
-});
+});*/
 /*var newTodo=new Todo({
   text: 'Cook dinner'
 });
