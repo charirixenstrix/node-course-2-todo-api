d5347f5398e315082c2f788615186d0048786db6
Charirixenstrix <charirixenstrix@gmail.com>
2017-08-17 12:15:03 +0200

    Add get /todos:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 var express=require('express');
 var bodyParser=require('body-parser');
+const {ObjectID}=require('mongodb');
 
 var {mongoose}=require('./db/mongoose');
 var {Todo}=require('./models/todo');
@@ -26,6 +27,24 @@ app.get('/todos', (req, res)=>{
   }, (e)=>{
     res.status(400).send(e);
   });
+});
+
+app.get('/todos/:id', (req, res)=>{
+  var id=req.params.id;
+
+  if(!ObjectID.isValid(id)){
+    console.log('ID not valid');
+    return res.status(404).send({uzi: 'Hibás id!'});
+  }
+  Todo.findById(id).then((todo)=>{
+    if(!todo){
+      return res.status(400).send({uzi: 'Nincs adat'});
+    }
+    res.send({uzi: 'ok', todo});
+  }).catch((e)=>{
+    return res.status(404).send({uzi: 'Hiba történt a lekérdezésben'});
+  });
+
 });
 
 app.listen(3000, ()=>{
