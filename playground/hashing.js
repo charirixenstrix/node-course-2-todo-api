28047b5509255fffdd1f666d696afc9a56c4e838
Charirixenstrix <charirixenstrix@gmail.com>
2017-08-21 14:47:02 +0200

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,6 +1,21 @@
 const {SHA256}=require('crypto-js');
 const jwt=require('jsonwebtoken');
+const bcrypt=require('bcryptjs');
 
+var password='123abc!';
+bcrypt.genSalt(10, (err, salt)=>{
+  bcrypt.hash(password, salt, (err, hash)=>{
+    console.log(hash);
+  });
+});
+
+var hashedPassword='$2a$10$JOLJipVAWQFuLYfGJM.3IOqgwDlSgkmQmM/QmDEChHFZSFbhfGFkq';
+
+bcrypt.compare(password, hashedPassword, (err, res)=>{
+  console.log(res);
+});
+
+/*
 var data={id: 10};
 
 var token=jwt.sign(data, '123abc');
@@ -32,3 +47,4 @@ else{
   console.log('Data was changed. Do not trust!');
 }
 //jwt megcsinálja ezt
+*/
